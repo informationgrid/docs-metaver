6cc64b7042b8ddb5f7082e591a334d360991e283
Torsten Wolff <37546346+tomiwo@users.noreply.github.com>
2024-04-11 09:28:47 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'METAVER - Bedienungsanleitung (IGE-NG)'
+project = 'IGE-NG Bedienungsanleitung Profil METAVER'
 copyright = '2021-{}, wemove'.format(
     datetime.datetime.now().year
 )
